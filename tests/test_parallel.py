a7932c2f405850cabdce9d4bd1af2f7e7bfcd2e3
ajax23 <hamzeh_kraus@web.de>
2020-03-27 15:42:18 +0100

    Update coverage handling

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -8,20 +8,20 @@ from porems.molecule import Molecule
 from porems.essentials import *
 from porems.store import Store
 from porems.pattern import *
-from porems.cube import Cube
+from porems.dice import Dice
 from porems.matrix import Matrix
 from porems.pore import Pore
 
 
 class UserModelCase(unittest.TestCase):
     ########
-    # Cube #
+    # Dice #
     ########
-    def test_cube_parallel(self):
-        cube = Cube(BetaCristobalit().generate([2, 2, 2], "z"), 0.4, True)
+    def test_dice_parallel(self):
+        dice = Dice(BetaCristobalit().generate([2, 2, 2], "z"), 0.4, True)
 
-        self.assertEqual(len(cube.find_parallel(None, ["Si", "O"], 0.155, 0.005)), 192)
-        self.assertEqual(len(cube.find_parallel(None, ["O", "Si"], 0.155, 0.005)), 384)
+        self.assertEqual(len(dice.find_parallel(None, ["Si", "O"], 0.155, 0.005)), 192)
+        self.assertEqual(len(dice.find_parallel(None, ["O", "Si"], 0.155, 0.005)), 384)
 
 
 if __name__ == '__main__':
