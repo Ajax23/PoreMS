4ef38a2507ea647be604ff80267dc61ddf5d015a
ajax23 <github@kraushh.de>
2021-02-09 15:35:06 +0100

    Create Development version 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="porems",
-    version="0.2.1",
+    version="0.2.2",
     author="Hamzeh Kraus",
     author_email="kraus@itt.uni-stuttgart.de",
     description="Pore Generator for Molecular Simulations.",
